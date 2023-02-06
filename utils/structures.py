b45de8ee7a62c8eee50f993cb95a6fb427e1d01f
FilipSzweda <filip.szweda1@gmail.com>
2023-02-06 22:20:39 +0100

    FIX PROGRAM BREAKING ISSUE FOR BOOMER PYTHON VERSIONS

--- utils/structures.py
+++ utils/structures.py
@@ -1,5 +1,8 @@
+from typing import List, Tuple
+
+
 class Question:
-    def __init__(self, q: str, a: list[tuple:2], c: list[str]):
+    def __init__(self, q: str, a: List[Tuple], c: List[str]):
         self.q = q
         self.a = a
         self.c = c
